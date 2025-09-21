331c4721d9307b97fb50da79d034b50a20ec0473
InuDeveloper <daviddevlife@gmail.com>
2025-09-21 19:35:41 -0400

    Learning about variables in Java - Planning an simple beginning project to learn further

--- src/main/java/lab/InuzDev/Main.java
+++ src/main/java/lab/InuzDev/Main.java
@@ -3,6 +3,10 @@ package lab.InuzDev; // We need to research how to improve this
 public class Main {
    public static void main(String[] args) {
       String name = "John";
-      System.out.println("Hello " + name);
+      System.out.println("Hello, " + name);
+
+      // I'm going to do some math with Java
+      int number = 100;
+      System.out.print("This is a number: " + number);
    }
 }
